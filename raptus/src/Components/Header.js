2e9009fe311b9cda79a3f67007614f77f91dc672
Liv Sarah <sarahansnes@gmail.com>
2020-06-26 18:36:38 +0200

    New header with dynamic content

--- raptus/src/Components/Header.js
+++ raptus/src/Components/Header.js
@@ -5,7 +5,16 @@ function Header() {
   return (
       <div className="Header">
         <header>
-            <h1>Raptusfestivalen</h1>
+            <div className="overlay">
+                <div className="logo-wrapper">
+                    <img className="talk-bubble" src="https://static.wixstatic.com/media/8725a4_c6fadf9d63204a61a35a9ff9ac11dcb0~mv2.png/v1/fill/w_376,h_142,al_c,q_85,usm_0.66_1.00_0.01/st%C3%B8rste_festival.webp"/>
+                    <img className="logo" src="https://static.wixstatic.com/media/8725a4_50db4fc243d44457a0c0f24b35ab1836~mv2.png/v1/fill/w_600,h_231,al_c,q_85,usm_0.66_1.00_0.01/Logo%20tagline.webp"/>
+                </div>
+                <h3>MINIRAPTUS 12.september 2020</h3>
+                <p>Grunnet alt som skjer rundt Covid-19 vil det ikke bli avholdt vanlig Raptus i år. I stedet vil det bli Mini-Raptus på Bergen Offentlige Bibliotek 12.september 2020. Mer info kommer!</p>
+                <br/>
+                <button>LES MER</button>
+            </div>
         </header>
     </div>
   )
