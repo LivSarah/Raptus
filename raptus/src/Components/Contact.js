8fea89cd6d8caa818f6c6edc6df49d9c8b5ff7e9
Liv Sarah <sarahansnes@gmail.com>
2020-06-27 00:01:03 +0200

    Added basic elements to Contact component

---
+++ raptus/src/Components/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Styles/Contact.scss'
+
+function Contact() {
+  return (
+    <div className="Contact">
+        <span className="title">Kontakt Oss</span>
+        <div className="contact-wrapper">
+            <div className="info">
+                <span>
+                Festivalen arrangeres i Bergen sentrum.<br/>
+                Vi kommer tilbake med lokale senere.<br/><br/>
+                Du finner oss på både Facebook og Instagram.
+                </span>
+            </div>
+            <div classNamme="contact-form">
+                <p>Kommer kontaktskjema her</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
