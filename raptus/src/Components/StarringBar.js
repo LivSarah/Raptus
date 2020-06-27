81533dbbde00d3dc4b732766e4dd8c329e5ad768
Liv Sarah <sarahansnes@gmail.com>
2020-06-27 03:45:14 +0200

    Added some text to explain function of component

--- raptus/src/Components/StarringBar.js
+++ raptus/src/Components/StarringBar.js
@@ -1,1 +1,6 @@
 import React from 'react';
+
+/*
+list of 3 guest-id's from database to showcase in the starring-bar on the front page, using name, image and title. 
+4th and last frame should be "Se alle gjestene". 
+*/
