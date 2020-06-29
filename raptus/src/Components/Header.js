1606b2f3aeeb78e1b499a5259ebc1e46630075f5
Liv Sarah <sarahansnes@gmail.com>
2020-06-29 23:08:16 +0200

    Changed image sources to local files

--- raptus/src/Components/Header.js
+++ raptus/src/Components/Header.js
@@ -7,8 +7,8 @@ function Header() {
         <header>
             <div className="overlay">
                 <div className="logo-wrapper">
-                    <img className="talk-bubble" src="https://static.wixstatic.com/media/8725a4_c6fadf9d63204a61a35a9ff9ac11dcb0~mv2.png/v1/fill/w_376,h_142,al_c,q_85,usm_0.66_1.00_0.01/st%C3%B8rste_festival.webp"/>
-                    <img className="logo" src="https://static.wixstatic.com/media/8725a4_50db4fc243d44457a0c0f24b35ab1836~mv2.png/v1/fill/w_600,h_231,al_c,q_85,usm_0.66_1.00_0.01/Logo%20tagline.webp"/>
+                    <img className="talk-bubble" src={require('../Assets/Images/speech-bubble.webp')} alt="raptus-logo-speech-bubble"/>
+                    <img className="logo" src={require('../Assets/Images/logo.webp')} alt="raptus-logo"/>
                 </div>
                 <h3>MINIRAPTUS 12.september 2020</h3>
                 <p>Grunnet alt som skjer rundt Covid-19 vil det ikke bli avholdt vanlig Raptus i år. I stedet vil det bli Mini-Raptus på Bergen Offentlige Bibliotek 12.september 2020. Mer info kommer!</p>
