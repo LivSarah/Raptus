bff867819d6c38509c2b48d0ebb2c2357f8ff0ff
Liv Sarah <sarahansnes@gmail.com>
2020-07-02 23:22:35 +0200

    Added footer with copyright info

---
+++ raptus/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles/Footer.scss';
+
+function Footer() {
+    return (
+      <div className="Footer">
+          <p>Opphavsrett © 2020 · Alle rettigheter tilhører · Raptus</p>
+      </div>
+    )
+  }
+  
+  export default Footer;
