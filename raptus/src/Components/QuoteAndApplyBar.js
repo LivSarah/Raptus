dedb4236960beb44f22ce68e6caca031c5ca451a
Liv Sarah <sarahansnes@gmail.com>
2020-07-02 23:24:40 +0200

    Added some containers for placeholding

--- raptus/src/Components/QuoteAndApplyBar.js
+++ raptus/src/Components/QuoteAndApplyBar.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Styles/QuoteAndApplyBar.scss';
 
 function QuoteAndApplyBar() {
   return (
     <div className="QuoteAndApplyBar">
-
+        <div className="quote-container">
+            <p>KAKE</p>
+        </div>
+        <div className="divider"/>
+        <div className="apply-container">
+            <p>BADEKAKE</p>
+        </div>
     </div>
   )
 }
