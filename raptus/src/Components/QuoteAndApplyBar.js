2cd5b9b557a92e2a1f95ad14206e267309958e25
Liv Sarah <sarahansnes@gmail.com>
2020-07-03 03:55:21 +0200

    Added image and text to containers

--- raptus/src/Components/QuoteAndApplyBar.js
+++ raptus/src/Components/QuoteAndApplyBar.js
@@ -5,11 +5,15 @@ function QuoteAndApplyBar() {
   return (
     <div className="QuoteAndApplyBar">
         <div className="quote-container">
-            <p>KAKE</p>
+            <div className="bubble-and-quote">
+                <p className="quote">"Dette er et kjempebra sitat av en eller annen person!"</p>
+                <img src={require('../Assets/Images/speech-bubble.png')} alt="speech-bubble"/>
+            </div>
+            <p className="quote-owner"><b>Mike Collins</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Artist</p>
         </div>
         <div className="divider"/>
         <div className="apply-container">
-            <p>BADEKAKE</p>
+            <p>SØK FRIVILLIG, INTERVJUER OG STAND HER</p>
         </div>
     </div>
   )
