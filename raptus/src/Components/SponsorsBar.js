d9b2c7d9e17e5cf42f9d3b2e8f693bde87e7d8c7
Liv Sarah <sarahansnes@gmail.com>
2020-07-03 03:51:31 +0200

    New initial component

---
+++ raptus/src/Components/SponsorsBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles/SponsorsBar.scss';
+
+function SponsorsBar() {
+    return (
+      <div className="SponsorsBar">
+          <p>Våre sponsorer: </p>
+      </div>
+    )
+  }
+  
+  export default SponsorsBar;
