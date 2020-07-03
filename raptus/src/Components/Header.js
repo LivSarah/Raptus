71b52e8859b22cfd6c3829e4026ad5785397fbb3
Liv Sarah <sarahansnes@gmail.com>
2020-07-03 03:54:22 +0200

    Traded two-part logo for new one-part logo

--- raptus/src/Components/Header.js
+++ raptus/src/Components/Header.js
@@ -7,8 +7,7 @@ function Header() {
         <header>
             <div className="overlay">
                 <div className="logo-wrapper">
-                    <img className="talk-bubble" src={require('../Assets/Images/speech-bubble.webp')} alt="raptus-logo-speech-bubble"/>
-                    <img className="logo" src={require('../Assets/Images/logo.webp')} alt="raptus-logo"/>
+                    <img className="logo" src={require('../Assets/Images/logo.png')} alt="raptus-logo"/>
                 </div>
                 <h3>MINIRAPTUS 12.september 2020</h3>
                 <p>Grunnet alt som skjer rundt Covid-19 vil det ikke bli avholdt vanlig Raptus i år. I stedet vil det bli Mini-Raptus på Bergen Offentlige Bibliotek 12.september 2020. Mer info kommer!</p>
