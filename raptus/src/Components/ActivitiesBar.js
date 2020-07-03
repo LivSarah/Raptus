a06dcf51b44110ec708655b357a1e2508552fb2c
Liv Sarah <sarahansnes@gmail.com>
2020-07-03 03:53:29 +0200

    Minor typo-fix

--- raptus/src/Components/ActivitiesBar.js
+++ raptus/src/Components/ActivitiesBar.js
@@ -5,7 +5,7 @@ import {Button} from 'react-bootstrap';
 function ActivitiesBar() {
   return (
     <div className="ActivitiesBar">
-        <span>Cosplay, Panel<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stands &amp; Mer</span>
+        <span>Cosplay, Panel,<br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stands &amp; Mer</span>
         <h4>Vi er tilbake 10.-12.sep 2021 med 25års jubileum!</h4>
         <p>I 2021 feirer Raptus sitt 25 års jubileum, og for at dette skal bli spektakulært trenger vi både 
         intervjuere til å sitte på scenen med våre gjester, frivillige til å hjelpe til under festivalen, og 
