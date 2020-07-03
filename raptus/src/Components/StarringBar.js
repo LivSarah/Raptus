4f30b4fc93ad4907b326e4371b4da135cfc13f3f
Liv Sarah <sarahansnes@gmail.com>
2020-07-03 03:56:12 +0200

    Added dividers between images

--- raptus/src/Components/StarringBar.js
+++ raptus/src/Components/StarringBar.js
@@ -5,8 +5,11 @@ function StarringBar() {
   return (
     <div className="Starring-Bar">
         <a href="/"><img src={require('../Assets/Images/mike-perkins.jpg')} alt="mike-perkins"/></a>
+        <div className="divider"/>
         <a href="/"><img src={require('../Assets/Images/mike-collins.jpg')} alt="mike-collins"/></a>
+        <div className="divider"/>
         <a href="/"><img src={require('../Assets/Images/barry-kitson.jpg')} alt="barry-kitson"/></a>
+        <div className="divider"/>
         <a href="/"><img src={require('../Assets/Images/se-alle-gjester.jpg')} alt="view-all-guests"/></a>
     </div>
   )
