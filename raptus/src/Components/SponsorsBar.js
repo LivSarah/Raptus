750409a928d4fd02a3cda233556520e22a3c98fb
Liv Sarah <sarahansnes@gmail.com>
2020-07-20 09:51:48 +0200

    Added the sponsor logos

--- raptus/src/Components/SponsorsBar.js
+++ raptus/src/Components/SponsorsBar.js
@@ -4,7 +4,15 @@ import './Styles/SponsorsBar.scss';
 function SponsorsBar() {
     return (
       <div className="SponsorsBar">
-          <p>Våre sponsorer: </p>
+          <div className="content-wrapper">
+            <p className="our-sponsors">Våre<br/>sponsorer: </p>
+            <div className="divider" />
+            <img src={require('../Assets/Images/kulturraadet.png')} alt="kulturrådet-logo"/>
+            <div className="divider" />
+            <img src={require('../Assets/Images/biblioteket.png')} alt="biblioteket-logo"/>
+            <div className="divider" />
+            <img src={require('../Assets/Images/bergen-kommune.png')} alt="bergen-kommune-logo"/>
+          </div>
       </div>
     )
   }
