9a5f349052559ce75581fd7bf919552a113b3017
Liv Sarah <sarahansnes@gmail.com>
2020-07-20 10:06:12 +0200

    Changes in folder structure, and deleted unused classes

--- raptus/src/index.js
+++ raptus/src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import Header from './Components/Header';
 import Contact from './Components/Contact';
-import ActivitiesBar from './Components/ActivitiesBar';
-import StarringBar from './Components/StarringBar';
-import QuoteAndApplyBar from './Components/QuoteAndApplyBar';
+import ActivitiesBar from './Components/Frontpage/ActivitiesBar';
+import StarringBar from './Components/Frontpage/StarringBar';
+import QuoteAndApplyBar from './Components/Frontpage/QuoteAndApplyBar';
 import Footer from './Components/Footer';
-import SponsorsBar from './Components/SponsorsBar';
+import SponsorsBar from './Components/Frontpage/SponsorsBar';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
