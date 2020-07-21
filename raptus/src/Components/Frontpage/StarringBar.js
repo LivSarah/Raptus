5ba70952083f9a212b720a35d6a6221d5f289791
Liv Sarah <sarahansnes@gmail.com>
2020-07-21 14:49:41 +0200

    Starring guests are now rendered from a list - to later be collected from DB

--- raptus/src/Components/Frontpage/StarringBar.js
+++ raptus/src/Components/Frontpage/StarringBar.js
@@ -2,15 +2,24 @@ import React from 'react'
 import './Styles/StarringBar.scss';
 
 function StarringBar() {
+
+  const guests = [
+    {href: '/', alt: "mike-perkins", img: require('../../Assets/Images/mike-perkins.jpg'), name: "Mike Perkjins", profession: "Artist"},
+    {href: '/', alt: "mike-collins", img: require('../../Assets/Images/mike-collins.jpg'), name: "Mike Collins", profession: "Artist"},
+    {href: '/', alt: "barry-kitson", img: require('../../Assets/Images/barry-kitson.jpg'), name: "Barry Kitson", profession: "Artist"}
+  ];
+
   return (
     <div className="Starring-Bar">
-        <a href="/"><img src={require('../../Assets/Images/mike-perkins.jpg')} alt="mike-perkins"/></a>
-        <div className="divider"/>
-        <a href="/"><img src={require('../../Assets/Images/mike-collins.jpg')} alt="mike-collins"/></a>
-        <div className="divider"/>
-        <a href="/"><img src={require('../../Assets/Images/barry-kitson.jpg')} alt="barry-kitson"/></a>
-        <div className="divider"/>
-        <a href="/"><img src={require('../../Assets/Images/se-alle-gjester.jpg')} alt="view-all-guests"/></a>
+      {guests.map((value, index) => {
+        return <div className="guest-container">
+                <a href={value.href} key={index}><img src={value.img} alt={value.alt}/></a>
+                <div className="divider"/>
+              </div>
+        })}
+        <div className="guest-container">
+          <a href="/"><img src={require('../../Assets/Images/se-alle-gjester.jpg')} alt={"view-all-guests"}/></a>
+        </div>
     </div>
   )
 }
