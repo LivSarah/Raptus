892dbfa78a0015f22309053a7a5fccf5b614a214
Liv Sarah <sarahansnes@gmail.com>
2020-07-22 14:37:55 +0200

    Added starring-cloud!

--- raptus/src/Components/Frontpage/StarringBar.js
+++ raptus/src/Components/Frontpage/StarringBar.js
@@ -11,6 +11,18 @@ function StarringBar() {
 
   return (
     <div className="Starring-Bar">
+      <div className="starring-cloud">
+        <p>Starring:</p>
+        <svg viewBox='0 0 105 105'  >
+        <path d='M 25,60 
+                a 20,20 1 0,0 0,40 
+                h 50 
+                a 20,20 1 0,0 0,-40 
+                a 10,10 1 0,0 -15,-10 
+                a 15,15 1 0,0 -35,10  
+                z' />
+        </svg>
+      </div>
       {guests.map((value, index) => {
         return <div className="guest-container">
                 <a href={value.href} key={index}>
