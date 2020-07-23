d7b42cc191b43b13c3800f60d0e93888ad4a8a46
Liv Sarah <sarahansnes@gmail.com>
2020-07-23 17:46:11 +0200

    Added basic logic and some link-tags to navigate views

--- raptus/src/Components/Header.js
+++ raptus/src/Components/Header.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import './Styles/Header.scss';
+import { Link } from "react-router-dom";
+
+export const Header = () => (
+        <div className="Header">
+            <Link to="/">Hjem</Link>
+
+            <Link to="/gjester">Gjester</Link>
+
+            <Link to="/program">Program</Link>
+        </div>
+);
