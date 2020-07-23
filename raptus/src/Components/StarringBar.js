b47a81b39045371908efc1e777d61acc06c5a687
Liv Sarah <sarahansnes@gmail.com>
2020-07-23 17:48:01 +0200

    Changed div to section and changed an image link

--- raptus/src/Components/StarringBar.js
+++ raptus/src/Components/StarringBar.js
@@ -6,21 +6,21 @@ function StarringBar() {
   const guests = [
     {href: '/', alt: "mike-perkins", img: require('../Assets/Images/mike-perkins.jpg'), name: "Mike Perkins", profession: "Illustratør", id: 1},
     {href: '/', alt: "mike-collins", img: require('../Assets/Images/mike-collins.jpg'), name: "Mike Collins", profession: "Illustratør", id: 2},
-    {href: '/', alt: "barry-kitson", img: require('../Assets/Images/barry-kitson.jpg'), name: "Barry Kitson", profession: "Illustratør", id: 3}
+    {href: '/', alt: "barry-kitson", img: 'https://firebasestorage.googleapis.com/v0/b/raptus-62004.appspot.com/o/barry-kitson.jpg?alt=media', name: "Barry Kitson", profession: "Illustratør", id: 3}
   ];
 
   return (
-    <div className="Starring-Bar">
+    <section className="Starring-Bar">
       <div className="starring-cloud">
         <p>Starring:</p>
         <svg viewBox='0 0 105 105'  >
-        <path d='M 25,60 
-                a 20,20 1 0,0 0,40 
-                h 50 
-                a 20,20 1 0,0 0,-40 
-                a 10,10 1 0,0 -15,-10 
-                a 15,15 1 0,0 -35,10  
-                z' />
+          <path d='M 25,60 
+                  a 20,20 1 0,0 0,40 
+                  h 50 
+                  a 20,20 1 0,0 0,-40 
+                  a 10,10 1 0,0 -15,-10 
+                  a 15,15 1 0,0 -35,10  
+                  z' />
         </svg>
       </div>
       {guests.map((value) => {
@@ -36,7 +36,7 @@ function StarringBar() {
         <div className="guest-container">
           <a href="/"><img src={require('../Assets/Images/se-alle-gjester.jpg')} alt={"view-all-guests"}/></a>
         </div>
-    </div>
+    </section>
   )
 }
 
