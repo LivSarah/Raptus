c8ae2cde9f67b72202df739dd15709aa5b19d896
Liv Sarah <sarahansnes@gmail.com>
2020-07-23 17:38:18 +0200

    Added new empty views for placeholding

---
+++ raptus/src/Views/Guests.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Guests = () => (
+    <>
+        <h1>GJESTER</h1>
+    </>
+);
---
+++ raptus/src/Views/Program.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Program = () => (
+    <>
+        <h1>PROGRAM</h1>
+    </>
+);
