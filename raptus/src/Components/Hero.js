838d2dcedb609a24cb438839cb96e75f71ab8bbc
Liv Sarah <sarahansnes@gmail.com>
2020-07-23 17:47:13 +0200

    Changed classname from header to hero

--- raptus/src/Components/Hero.js
+++ raptus/src/Components/Hero.js
@@ -1,21 +1,19 @@
 import React from 'react'
-import './Styles/Header.scss'
+import './Styles/Hero.scss'
 
 function Hero() {
   return (
-    <div className="Header">
-        <header>
-            <div className="overlay">
-                <div className="logo-wrapper">
-                    <img className="logo" src={require('../Assets/Images/logo.png')} alt="raptus-logo"/>
-                </div>
-                <h3>MINIRAPTUS 12.september 2020</h3>
-                <p>Grunnet alt som skjer rundt Covid-19 vil det ikke bli avholdt vanlig Raptus i år. I stedet vil det bli Mini-Raptus på Bergen Offentlige Bibliotek 12.september 2020. Mer info kommer!</p>
-                <br/>
-                <button>LES MER</button>
-            </div>
-        </header>
-    </div>
+      <header className="Hero">
+          <div className="overlay">
+              <div className="logo-wrapper">
+                  <img className="logo" src={require('../Assets/Images/logo.png')} alt="raptus-logo"/>
+              </div>
+              <h3>MINIRAPTUS 12.september 2020</h3>
+              <p>Grunnet alt som skjer rundt Covid-19 vil det ikke bli avholdt vanlig Raptus i år. I stedet vil det bli Mini-Raptus på Bergen Offentlige Bibliotek 12.september 2020. Mer info kommer!</p>
+              <br/>
+              <button>LES MER</button>
+          </div>
+      </header>
   )
 }
 
