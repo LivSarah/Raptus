40a67e75c82d92db7b1afdeb46a53def6be46768
Liv Sarah <sarahansnes@gmail.com>
2020-07-24 20:38:08 +0200

    Temporary trying to use the GuestsCollection in stead of StarringBar to preview guests

--- raptus/src/Views/Home.js
+++ raptus/src/Views/Home.js
@@ -3,11 +3,20 @@ import Hero from '../Components/Hero';
 import StarringBar from '../Components/StarringBar';
 import ActivitiesBar from '../Components/ActivitiesBar';
 import QuoteAndApplyBar from '../Components/QuoteAndApplyBar';
+import { GuestsCollection } from '../Components/GuestsCollection';
+import { Person } from '../Components/Person'
 
 export const Home = () => (
     <>
         <Hero />
-        <StarringBar />
+        {/*<StarringBar />*/}
+        <GuestsCollection>
+            {(guests) => (
+                guests.map((guest) => (
+                    <Person image={guest.image} href="#" />
+                ))
+            )}
+        </GuestsCollection>
         <ActivitiesBar />
         <QuoteAndApplyBar />
     </>
