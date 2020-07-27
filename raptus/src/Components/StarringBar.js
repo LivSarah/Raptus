192fce85278746061f08d5acc4b5a4a67a360933
Liv Sarah <sarahansnes@gmail.com>
2020-07-27 16:12:08 +0200

    Major change: removed hardcoded data and implemented guest data collected from database through GuestCollection component

--- raptus/src/Components/StarringBar.js
+++ raptus/src/Components/StarringBar.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import './Styles/StarringBar.scss';
+import { GuestsCollection } from '../Components/GuestsCollection';
+import { Person } from '../Components/Person'
 
 function StarringBar() {
-
-  const guests = [
-    {href: '/', alt: "mike-perkins", img: require('../Assets/Images/mike-perkins.jpg'), name: "Mike Perkins", profession: "Illustratør", id: 1},
-    {href: '/', alt: "mike-collins", img: require('../Assets/Images/mike-collins.jpg'), name: "Mike Collins", profession: "Illustratør", id: 2},
-    {href: '/', alt: "barry-kitson", img: 'https://firebasestorage.googleapis.com/v0/b/raptus-62004.appspot.com/o/barry-kitson.jpg?alt=media', name: "Barry Kitson", profession: "Illustratør", id: 3}
-  ];
 
   return (
     <section className="Starring-Bar">
@@ -23,19 +19,18 @@ function StarringBar() {
                   z' />
         </svg>
       </div>
-      {guests.map((value) => {
-        return <div className="guest-container" key={value.id}>
-                <a href={value.href}>
-                  <div className="color-tint"/>
-                  <img src={value.img} alt={value.alt}/>
-                  <p className="guest-name">{value.name}</p>
-                  <p className="guest-profession">{value.profession}</p>
-                </a>
-              </div>
-        })}
-        <div className="guest-container">
+      <div className="guests-grid">
+        <GuestsCollection>
+          {(guests) => (
+              guests.map((guest) => (
+                  <Person name={guest.name} profession={guest.profession} image={guest.image} href="/" key={guest.id}/>
+              ))
+          )}
+        </GuestsCollection>
+        <div className="view-all-guests">
           <a href="/"><img src={require('../Assets/Images/se-alle-gjester.jpg')} alt={"view-all-guests"}/></a>
         </div>
+      </div>
     </section>
   )
 }
