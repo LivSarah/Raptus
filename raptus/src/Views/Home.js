0e51724277464a8e14716cfdd2c44eb2125a1ab2
Liv Sarah <sarahansnes@gmail.com>
2020-07-28 23:39:34 +0200

    Removed unused imports

--- raptus/src/Views/Home.js
+++ raptus/src/Views/Home.js
@@ -3,8 +3,6 @@ import Hero from '../Components/Hero';
 import StarringBar from '../Components/StarringBar';
 import ActivitiesBar from '../Components/ActivitiesBar';
 import QuoteAndApplyBar from '../Components/QuoteAndApplyBar';
-import { GuestsCollection } from '../Components/GuestsCollection';
-import { Person } from '../Components/Person'
 
 export const Home = () => (
     <>
