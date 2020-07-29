58e5a4312a9c972413f0a7f53e959e26f6ed8b21
Liv Sarah <sarahansnes@gmail.com>
2020-07-29 18:34:48 +0200

    Added component argument and used it in conditionally render tag

--- raptus/src/Components/Person.js
+++ raptus/src/Components/Person.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Styles/Person.scss';
 
-export const Person = ({image, href, overlay, name, profession, key}) => {
-    const tag = href ? 'a' : 'span';
+export const Person = ({image, href, overlay, name, profession, clickable}) => {
+    const tag = clickable ? 'a' : 'span';
 
-    return React.createElement(tag, {href}, [
-            <div className="Person">
-                <div className="color-tint"/>
-                <img src={image} alt={name}/>
-                <p className="name">{name}</p>
-                <p className="profession">{profession}</p>
-            </div>
-    ])
+    return React.createElement(tag, {href}, (
+        <div className="Person">
+            <div className="color-tint"/>
+            <img src={image} alt={name}/>
+            <p className="name">{name}</p>
+            <p className="profession">{profession}</p>
+        </div>
+    ));
 }
