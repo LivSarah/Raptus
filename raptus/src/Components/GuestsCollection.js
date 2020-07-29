20ac43a6a5e5a647014e1deded34e9fde20a265c
Liv Sarah <sarahansnes@gmail.com>
2020-07-29 18:33:36 +0200

    Some refactoring to make code easier to read

--- raptus/src/Components/GuestsCollection.js
+++ raptus/src/Components/GuestsCollection.js
@@ -7,21 +7,13 @@ export const GuestsCollection = withFirebase(({ firebase, children, starring }) 
     useEffect(() => {
         (async () => {
             try {
-                if (starring) {           
-                    const snapshot = await firebase.starringGuests();
-                                    
-                    const guests = snapshot.docs.map((doc) => (
-                        doc.data()
-                    ));
-                    setGuests(guests);
-                } else {
-                    const snapshot = await firebase.guests();
+                const snapshot = await (starring ? firebase.starringGuests() : firebase.guests());
+                
+                const guests = snapshot.docs.map((doc) => (
+                    doc.data()
+                ));
+                setGuests(guests);
 
-                    const guests = snapshot.docs.map((doc) => (
-                        doc.data()
-                    ));
-                    setGuests(guests);
-                }
             } catch (error) {
                 console.error(error)
             }
