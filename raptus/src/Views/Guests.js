43d4b727f177462c03c410dd018c116a7ffc9ef7
Liv Sarah <sarahansnes@gmail.com>
2020-07-29 17:01:53 +0200

    Added grid with guests from database

--- raptus/src/Views/Guests.js
+++ raptus/src/Views/Guests.js
@@ -1,10 +1,23 @@
 import React from "react";
 import './Guests.scss';
+import { GuestsCollection } from '../Components/GuestsCollection';
+import { Person } from '../Components/Person'
 
 export const Guests = () => (
     <section className="Guests">
         <header>
-            <h1>Årets GJESTER</h1>
+            <h4>Årets</h4>
+            <h1>GJESTER</h1>
         </header>
+
+        <div className="guests-grid">
+            <GuestsCollection>
+              {(guests) => (
+                  guests.map((guest) => (
+                      <Person props={guest}/>
+                ))
+              )}
+            </GuestsCollection>
+        </div>
     </section>
 );
