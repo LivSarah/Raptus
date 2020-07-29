61311578c8990a0458c6613ea73bc336f0ccff45
Liv Sarah <sarahansnes@gmail.com>
2020-07-29 17:58:17 +0200

    Collection can now return only starring guests, based on input argument

--- raptus/src/Components/GuestsCollection.js
+++ raptus/src/Components/GuestsCollection.js
@@ -1,19 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import { withFirebase } from './Firebase';
 
-export const GuestsCollection = withFirebase(({ firebase, children }) => {
+export const GuestsCollection = withFirebase(({ firebase, children, starring }) => {
     const [guests, setGuests] = useState([]);
 
     useEffect(() => {
         (async () => {
             try {
-                const snapshot = await firebase.guests();
+                if (starring) {           
+                    const snapshot = await firebase.starringGuests();
+                                    
+                    const guests = snapshot.docs.map((doc) => (
+                        doc.data()
+                    ));
+                    setGuests(guests);
+                } else {
+                    const snapshot = await firebase.guests();
 
-                const guests = snapshot.docs.map((doc) => (
-                    doc.data()
-                ))
-
-                setGuests(guests)
+                    const guests = snapshot.docs.map((doc) => (
+                        doc.data()
+                    ));
+                    setGuests(guests);
+                }
             } catch (error) {
                 console.error(error)
             }
