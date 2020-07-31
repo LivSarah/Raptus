3278437ff2aecf5ffbc99b4ec4b5aeb2b31f1a1a
Liv Sarah <sarahansnes@gmail.com>
2020-07-31 12:58:58 +0200

    Changed from a-tag to Link-tag

--- raptus/src/Components/Person.js
+++ raptus/src/Components/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Styles/Person.scss';
+import { Link } from "react-router-dom";
 
-export const Person = ({image, href, overlay, name, profession, clickable}) => {
-    const tag = clickable ? 'a' : 'span';
+export const Person = ({image, to=undefined, name, profession, clickable}) => {
+    const tag = clickable ? Link : 'span';
 
-    return React.createElement(tag, {href}, (
+    return React.createElement(tag, {to}, (
         <div className="Person">
             <div className="color-tint"/>
             <img src={image} alt={name}/>
