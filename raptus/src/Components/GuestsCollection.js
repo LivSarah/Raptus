c900759acc8d2d23de8a5f0166cf4bc48b49f907
Liv Sarah <sarahansnes@gmail.com>
2020-07-31 12:58:31 +0200

    Refacored a lot, and moved most logic over to a utils-class

--- raptus/src/Components/GuestsCollection.js
+++ raptus/src/Components/GuestsCollection.js
@@ -1,24 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import { withFirebase } from './Firebase';
+import { usePromise } from '../Utils/usePromise';
 
 export const GuestsCollection = withFirebase(({ firebase, children, starring }) => {
-    const [guests, setGuests] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const snapshot = await (starring ? firebase.starringGuests() : firebase.guests());
+    const guests = usePromise(starring ? firebase.starringGuests() : firebase.guests(), []);
                 
-                const guests = snapshot.docs.map((doc) => (
-                    doc.data()
-                ));
-                setGuests(guests);
-
-            } catch (error) {
-                console.error(error)
-            }
-        })()
-    }, [])
     return (
         <>
             {children(guests)}
