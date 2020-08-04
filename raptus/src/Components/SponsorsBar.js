6c424d9cda861c6b466044537cd876b5c338d425
Liv Sarah <sarahansnes@gmail.com>
2020-08-04 02:45:53 +0200

    Removed redundant divider-elements, to later fix with css instead

--- raptus/src/Components/SponsorsBar.js
+++ raptus/src/Components/SponsorsBar.js
@@ -6,11 +6,8 @@ function SponsorsBar() {
       <section className="SponsorsBar">
           <div className="content-wrapper">
             <p className="our-sponsors">Våre<br/>sponsorer: </p>
-            <div className="divider" />
             <img src={require('../Assets/Images/kulturraadet.png')} alt="kulturrådet-logo"/>
-            <div className="divider" />
             <img src={require('../Assets/Images/biblioteket.png')} alt="biblioteket-logo"/>
-            <div className="divider" />
             <img src={require('../Assets/Images/bergen-kommune.png')} alt="bergen-kommune-logo"/>
           </div>
       </section>
