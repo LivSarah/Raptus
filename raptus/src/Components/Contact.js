8568a691b61409d4b8f2e4b90fa4d5a938b715e6
Liv Sarah <sarahansnes@gmail.com>
2020-08-04 02:42:36 +0200

    Edited tag from span to p

--- raptus/src/Components/Contact.js
+++ raptus/src/Components/Contact.js
@@ -8,11 +8,11 @@ function Contact() {
         <span className="title">Kontakt Oss</span>
         <div className="contact-wrapper">
             <div className="info">
-                <span>
+                <p>
                 Festivalen arrangeres i Bergen sentrum.<br/>
                 Vi kommer tilbake med lokale senere.<br/><br/>
                 Du finner oss på både Facebook og Instagram.
-                </span>
+                </p>
                 <div className="social-media">
                   <a href="https://www.facebook.com/Raptuscomics/"><img src={require('../Assets/Images/facebook-icon.webp')} alt="facebook-logo"/></a>
                   <a href="https://www.instagram.com/raptusfestivalen/"><img src={require('../Assets/Images/instagram-icon.webp')} alt="instagram-logo"/></a>
