60aefb5ad3b97f723f376ab5a7b82c0d4689e3e9
Liv Sarah <sarahansnes@gmail.com>
2020-08-04 02:43:06 +0200

    Added the new logo for use on smaller screens

--- raptus/src/Components/Hero.js
+++ raptus/src/Components/Hero.js
@@ -3,22 +3,21 @@ import './Styles/Hero.scss'
 
 function Hero() {
   return (
-      <header className="Hero">
-          <div className="overlay">
-              <div className="logo-wrapper">
-                  <img className="logo" src={require('../Assets/Images/logo.png')} alt="raptus-logo"/>
-              </div>
-              <div className="information-container">
-                    <h2>MINIRAPTUS 12.september 2020</h2>
-                    <h3>Grunnet alt som skjer rundt 
-                        Covid-19 vil det ikke bli avholdt 
-                        vanlig Raptus i år. I stedet vil 
-                        det bli Mini-Raptus på Bergen Offentlige 
-                        Bibliotek 12.september 2020. Mer info kommer!</h3>
-                    <button>LES MER</button>
-              </div>
-          </div>
-      </header>
+    <header className="Hero">
+        <div className="overlay">
+            <img className="logo" src={require('../Assets/Images/logo.png')} alt="raptus-logo"/>
+            <img className="logo-small" src={require('../Assets/Images/logoSmall.png')} alt="raptus-logo"/>
+            <div className="information-container">
+                <h2>MINIRAPTUS 12.september 2020</h2>
+                <h3>Grunnet alt som skjer rundt 
+                    Covid-19 vil det ikke bli avholdt 
+                    vanlig Raptus i år. I stedet vil 
+                    det bli Mini-Raptus på Bergen Offentlige 
+                    Bibliotek 12.september 2020. Mer info kommer!</h3>
+                <button>LES MER</button>
+            </div>
+        </div>
+    </header>
   )
 }
 
