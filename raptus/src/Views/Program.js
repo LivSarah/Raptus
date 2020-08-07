0e88c68654ed97a411ec5eae46c673a5f8ada640
Liv Sarah <sarahansnes@gmail.com>
2020-08-07 23:31:02 +0200

    Implemented the FestivalContentBar component

--- raptus/src/Views/Program.js
+++ raptus/src/Views/Program.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { PageHeader } from '../Components/PageHeader';
+import FestivalContentBar from '../Components/FestivalContentBar';
 
 export const Program = () => (
     <section className="Program">
         <PageHeader preTitle="Årets" title='PROGRAM'/>
+
+        <FestivalContentBar />
+
     </section>
 );
