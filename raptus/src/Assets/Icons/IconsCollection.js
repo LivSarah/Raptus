d518912ba2d62b00524d4f21a9dfa797ae6ed8bd
Liv Sarah <sarahansnes@gmail.com>
2020-08-10 23:50:26 +0200

    Js-file to access all icons in folder easy

---
+++ raptus/src/Assets/Icons/IconsCollection.js
@@ -0,0 +1,10 @@
+const autograph = require("./autograph.svg"); 
+const convention = require("./convention.svg")
+const famous = require("./famous.svg")
+const fortress = require("./fortress.svg")
+const kiosk = require("./kiosk.svg")
+const totoro = require("./totoro.svg")
+
+export const icons = {
+    autograph, convention, famous, fortress, kiosk, totoro
+}
