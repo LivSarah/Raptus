03d6320046c9f15ae8abe282f8a7c59d00f27db3
Liv Sarah <sarahansnes@gmail.com>
2020-08-10 23:53:41 +0200

    Replaced all svg-links with referances to the new js-file IconsCollection

--- raptus/src/Components/FestivalContentBar.js
+++ raptus/src/Components/FestivalContentBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Styles/FestivalContentBar.scss';
+import { icons } from '../Assets/Icons/IconsCollection';
 
 function FestivalContentBar() {
     const gridElements = [
-        {title: 'Programposter', text: "I'm a paragraph. Click here to add your own text and edit me.", svg: '../Assets/Icons/convention.svg'},
-        {title: 'Cosplay', text: "Vi har både cosplaykonurranse og photoshoot for våre utkledde deltakere.", svg: '../Assets/Icons/totoro.svg'},
-        {title: 'Eventyrhulen', text: "Et område for de små å leke, tegne og ha det gøy.", svg: '../Assets/Icons/fortress.svg'},
-        {title: 'Signeringer', text: "Im a paragraph. Click here to add your own text and edit me.", svg: '../Assets/Icons/autograph.svg'},
-        {title: 'Artists Alley', text: "I'm a paragraph. Click here to add your own text and edit me.", svg: '../Assets/Icons/famous.svg'},
-        {title: 'Stands', text: "I'm a paragraph. Click here to add your own text and edit me.", svg: '../Assets/Icons/kiosk.svg'}
+        {title: 'Programposter', text: "I'm a paragraph. Click here to add your own text and edit me.", svg: icons.convention},
+        {title: 'Cosplay', text: "Vi har både cosplaykonurranse og photoshoot for våre utkledde deltakere.", svg: icons.totoro},
+        {title: 'Eventyrhulen', text: "Et område for de små å leke, tegne og ha det gøy.", svg: icons.fortress},
+        {title: 'Signeringer', text: "Im a paragraph. Click here to add your own text and edit me.", svg: icons.autograph},
+        {title: 'Artists Alley', text: "I'm a paragraph. Click here to add your own text and edit me.", svg: icons.famous},
+        {title: 'Stands', text: "I'm a paragraph. Click here to add your own text and edit me.", svg: icons.kiosk}
     ]
     
     return (
@@ -17,7 +18,7 @@ function FestivalContentBar() {
             <div className="grid">
                 {gridElements.map((element) => (
                     <div className="element" key={element.title}>
-                        <img className="icon" src={require(element.svg)} alt={element.title}/>
+                        <img className="icon" src={element.svg} alt={element.title}/>
                         <h4>{element.title}</h4>
                         <p className="text">{element.text}</p>
                     </div>
