95739df54267a324906fb57892544e305b286682
Liv Sarah <sarahansnes@gmail.com>
2020-08-11 23:40:25 +0200

    Implemented new component TimescheduleColumn to show the festival program

--- raptus/src/Views/Program.js
+++ raptus/src/Views/Program.js
@@ -2,6 +2,8 @@ import React from "react";
 import './Program.scss'
 import { PageHeader } from '../Components/PageHeader';
 import FestivalContentBar from '../Components/FestivalContentBar';
+import TimescheduleColumn from '../Components/TimescheduleColumn';
+
 
 export const Program = () => (
     <section className="Program">
@@ -10,7 +12,15 @@ export const Program = () => (
         <FestivalContentBar />
 
         <article className="festival-schedule">
-
+            <TimescheduleColumn dayAndRoom="fridayRoom1" />
+            <TimescheduleColumn dayAndRoom="fridayRoom2"/>
+            <TimescheduleColumn dayAndRoom="fridayRoom3"/>
+            <TimescheduleColumn dayAndRoom="saturdayRoom1"/>
+            <TimescheduleColumn dayAndRoom="saturdayRoom2"/>
+            <TimescheduleColumn dayAndRoom="saturdayRoom3"/>
+            <TimescheduleColumn dayAndRoom="sundayRoom1"/>
+            <TimescheduleColumn dayAndRoom="sundayRoom2"/>
+            <TimescheduleColumn dayAndRoom="sundayRoom3"/>        
         </article>
 
     </section>
