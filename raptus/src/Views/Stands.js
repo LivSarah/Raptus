69cdc411656e090e5a58bfba9b65f205018605f3
Liv Sarah <sarahansnes@gmail.com>
2020-08-13 23:15:19 +0200

    Added StandsCollection-component to display previews of all stands from databse

--- raptus/src/Views/Stands.js
+++ raptus/src/Views/Stands.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import './Stands.scss';
+import { StandsCollection } from '../Components/StandsCollection';
+import { Person } from '../Components/Person';
 import { PageHeader } from '../Components/PageHeader';
 
 export const Stands = () => (
-    <section>
+    <section className="Stands">
         <PageHeader preTitle="Årets" title='STANDS' />
+
+        <div className="stands-grid">
+            <StandsCollection>
+              {(stands) => (
+                  stands.map((stand) => (
+                      <Person 
+                        name={stand.name} 
+                        image={stand.image} 
+                        to={`/stands/${stand.name}`}
+                        clickable={true} 
+                        key={stand.name}
+                      />
+                ))
+              )}
+            </StandsCollection>
+        </div>
     </section>
 );
