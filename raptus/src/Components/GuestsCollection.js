04c50da31a09644846375bbab8397a5df5a07ef0
Liv Sarah <sarahansnes@gmail.com>
2020-08-15 18:21:57 +0200

    Changed component to use FirebaseContext

--- raptus/src/Components/GuestsCollection.js
+++ raptus/src/Components/GuestsCollection.js
@@ -1,14 +1,13 @@
-import React from "react";
-import { withFirebase } from "./Firebase";
+import React, { useContext } from "react";
+import FirebaseContext from "../FirebaseContext";
 import { usePromise } from "../Utils/usePromise";
 
-export const GuestsCollection = withFirebase(
-  ({ firebase, children, starring }) => {
-    const guests = usePromise(
-      starring ? firebase.starringGuests() : firebase.guests(),
-      []
-    );
+export const GuestsCollection = ({ children, starring }) => {
+  const firebase = useContext(FirebaseContext);
+  const guests = usePromise(
+    starring ? firebase.starringGuests() : firebase.guests(),
+    []
+  );
 
-    return <>{children(guests)}</>;
-  }
-);
+  return <>{children(guests)}</>;
+};
