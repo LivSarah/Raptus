5e20d80e9230194171b51b26c5dfea24fa8ef3cd
Liv Sarah <sarahansnes@gmail.com>
2020-08-15 18:20:53 +0200

    Changed component to use FirebaseContext, and fixed some formatting with prettier

--- raptus/src/Views/Guest.js
+++ raptus/src/Views/Guest.js
@@ -1,29 +1,37 @@
-import React from 'react';
-import './Guest.scss';
-import { withFirebase } from '../Components/Firebase';
+import React, { useContext } from "react";
+import "./Guest.scss";
+import FirebaseContext from "../FirebaseContext";
 import { useParams } from "react-router-dom";
-import { usePromise } from '../Utils/usePromise';
-import Button from 'react-bootstrap/Button';
+import { usePromise } from "../Utils/usePromise";
+import Button from "react-bootstrap/Button";
 
-export const Guest = withFirebase(({ firebase }) => {
-    const { name } = useParams();
+export const Guest = () => {
+  const firebase = useContext(FirebaseContext);
+  const { name } = useParams();
 
-    const guest = usePromise(firebase.guestByName(name));
+  const guest = usePromise(firebase.guestByName(name));
 
-    if (guest) {
-        return (
-            <main className="Guest">
-                <div>
-                    <img className="guest-portrait" src={guest.image}></img>
-                    <Button className="view-website-button" href={guest.website} target="_blank" variant="outline-light">Se nettside</Button>                
-                </div>  
-                <div>
-                    <h3 className="guest-name">{guest.name}</h3>
-                    <p className="guest-bio">{guest.bio.replace(/\\n/g, '\n\n')}</p>
-                </div>
-            </main>
-          );    
-    } else {
-        return <p style={{textAlign: 'center'}}>Laster...</p>
-    }
-})
+  if (guest) {
+    return (
+      <main className="Guest">
+        <div>
+          <img className="guest-portrait" src={guest.image}></img>
+          <Button
+            className="view-website-button"
+            href={guest.website}
+            target="_blank"
+            variant="outline-light"
+          >
+            Se nettside
+          </Button>
+        </div>
+        <div>
+          <h3 className="guest-name">{guest.name}</h3>
+          <p className="guest-bio">{guest.bio.replace(/\\n/g, "\n\n")}</p>
+        </div>
+      </main>
+    );
+  } else {
+    return <p style={{ textAlign: "center" }}>Laster...</p>;
+  }
+};
