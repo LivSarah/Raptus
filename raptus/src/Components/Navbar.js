2044745b2fe00c761b9497a04e1cc34b63ff1b44
Liv Sarah <sarahansnes@gmail.com>
2020-08-17 23:32:19 +0200

    Changed component to have a hamburger manu when screen is less than 800px wide

--- raptus/src/Components/Navbar.js
+++ raptus/src/Components/Navbar.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import './Styles/Navbar.scss';
+import React from "react";
+import "./Styles/Navbar.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import classNames from "classnames";
+import { useRouteChange } from "../Utils/useRouteChange";
 
-export const Navbar = () => (
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavigation = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeNavigation = () => {
+      setIsOpen(false);
+  }
+
+  useRouteChange(() => {
+    closeNavigation();
+  });
+
+  return (
     <section className="Navbar">
+      <div
+        className={classNames({ "open-navigation": isOpen, veil: true })}
+        onClick={closeNavigation}
+      ></div>
+      <button onClick={toggleNavigation}>&#9776;</button>
+
+      <nav className={classNames({ "open-navigation": isOpen })}>
         <Link to="/">Hjem</Link>
 
         <Link to="/gjester">Gjester</Link>
@@ -15,5 +40,7 @@ export const Navbar = () => (
         <Link to="/stands">Stands</Link>
 
         <Link to="/frivillig">Frivillig</Link>
+      </nav>
     </section>
-);
+  );
+};
