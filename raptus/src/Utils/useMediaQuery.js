dbb6de3452a228ec3d3bc52332226d0c017fd930
Liv Sarah <sarahansnes@gmail.com>
2020-08-17 23:30:36 +0200

    New utils method for checking the media size

---
+++ raptus/src/Utils/useMediaQuery.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export const useMediaQuery = (mediaQueryString) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(mediaQueryString);
+
+    const listener = (event) => {
+      setMatches(event.matches);
+    };
+
+    listener(mediaQueryList);
+
+    mediaQueryList.addListener(listener);
+
+    return () => {
+      mediaQueryList.removeListener(listener);
+    };
+  }, []);
+
+  return matches;
+};
