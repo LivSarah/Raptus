10ecbbcab0983d41e115dbcf6af8611b393f33f0
Liv Sarah <sarahansnes@gmail.com>
2020-08-18 23:55:50 +0200

    Created a section for content, and immported stylesheet

--- raptus/src/Views/Cosplay.js
+++ raptus/src/Views/Cosplay.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { PageHeader } from "../Components/PageHeader";
+import './Cosplay.scss'
 
 export const Cosplay = () => (
   <main className="Cosplay">
     <PageHeader preTitle="Vi har" title="COSPLAY" />
+
+    <section className="content-wrapper">
+
+    </section>
   </main>
 );
