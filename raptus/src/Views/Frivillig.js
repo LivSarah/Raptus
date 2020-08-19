3818d6f691016dfebfefbdaeb1bb34cfa6244fea
Liv Sarah <sarahansnes@gmail.com>
2020-08-19 22:08:57 +0200

    Added section for content and imported style

--- raptus/src/Views/Frivillig.js
+++ raptus/src/Views/Frivillig.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { PageHeader } from "../Components/PageHeader/PageHeader";
+import './Frivillig.scss';
 
 export const Frivillig = () => (
   <main className="Frivillig">
     <PageHeader preTitle="Bli" title="FRIVILLIG" />
+
+    <section className="content-wrapper">
+
+    </section>
   </main>
 );
